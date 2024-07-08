1ddd834c15c45b079bba40350af6a0c47dbb9c7c
zhurongxiao <350058148@qq.com>
2024-07-08 15:27:40 +0800

    before 12节 添加评论区成功

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -9,8 +9,9 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import { slugify } from '../util/utilityFunctions'
 import authors from '../util/authors'
+import { DiscussionEmbed } from 'disqus-react'
 
-const SinglePost = ({ data }) => {
+const SinglePost = ({ data, pageContext }) => {
     const post = data.markdownRemark.frontmatter
     const post1 = data.file
     const fluid = getImage(post.image) // Access node.frontmatter.image inside the loop
@@ -18,6 +19,15 @@ const SinglePost = ({ data }) => {
 
     // const authorImageFluid = getImage(post1)
     const author = authors.find(x => x.name === post.author)
+
+    const baseUrl = 'https://gatsbytutorial.co.uk/'
+    const disqusShortname = 'https-gatsbytutorial-co-uk'
+    const disqusConfig = {
+        identifier: data.markdownRemark.id,
+        title: post.title,
+        url: baseUrl + pageContext.slug,
+    }
+
     return (
         <Layout pageTitle={post.title} postAuthor={author} authorImageFluid={authorImageFluid}>
             {/* <SEO title={post.title} /> */}
@@ -48,7 +58,72 @@ const SinglePost = ({ data }) => {
                 </CardBody>
 
             </Card>
-
+            <h3 className='text-center soclial-share-links'>Share this post</h3>
+            <div className="text-center social-share-links">
+                <ul>
+                    <li>
+                        <a
+                            href={
+                                'https://www.facebook.com/sharer/sharer.php?u=' +
+                                baseUrl +
+                                pageContext.slug
+                            }
+                            className="facebook"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i className="fab fa-facebook-f fa-2x" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href={
+                                'https://twitter.com/share?url=' +
+                                baseUrl +
+                                pageContext.slug +
+                                '&text=' +
+                                post.title +
+                                '&via' +
+                                'twitterHandle'
+                            }
+                            className="twitter"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i className="fab fa-twitter fa-2x" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href={
+                                'https://plus.google.com/share?url=' +
+                                baseUrl +
+                                pageContext.slug
+                            }
+                            className="google"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i className="fab fa-google fa-2x" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href={
+                                'https://www.linkedin.com/shareArticle?url=' +
+                                baseUrl +
+                                pageContext.slug
+                            }
+                            className="linkedin"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <i className="fab fa-linkedin fa-2x" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
 
         </Layout>
     )
